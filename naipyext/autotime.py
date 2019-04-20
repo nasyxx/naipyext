5b37156f62af22632c974e3144262162ae601661
Nasy <nasyxx+git@gmail.com>
2019-04-20 15:09:35 +0800

    Fixes Timer.

--- naipyext/autotime.py
+++ naipyext/autotime.py
@@ -37,8 +37,13 @@ license  : GPL-3.0+
 There are more things in heaven and earth, Horatio, than are dreamt.
  --  From "Hamlet"
 """
+# Standard Library
 import time
+
+# Other Packages
 from pendulum import duration as dt
+from IPython.core.interactiveshell import InteractiveShell
+
 
 class Timer:
     """A simply timer.
@@ -46,10 +51,11 @@ class Timer:
     Since the old one autotime that cannot easily install from pypi
     annoyed me,  I write my own one.
     """
+
     def __init__(self) -> None:
         """Initial timer."""
-        self.perf = 0
-        self.cput = 0
+        self.perf = 0.0
+        self.cput = 0.0
 
     def start(self) -> None:
         """Start timer."""
@@ -61,17 +67,20 @@ class Timer:
         perf = time.perf_counter() - self.perf
         cput = time.process_time() - self.cput
         if perf and cput and (perf > 1e-2 or cput > 1e-2):
-            print("Performance:", dt(perf).in_words())
-            print("Process:", dt(cput).in_words())
+            print("Performance:", dt(seconds=perf).in_words())
+            print("Process:", dt(seconds=cput).in_words())
+
 
 timer = Timer()
 
-def load_ipython_extension(ip):
-    "Load timer."
-    ip.events.register('pre_run_cell', timer.start)
-    ip.events.register('post_run_cell', timer.stop)
 
-def unload_ipython_extension(ip):
+def load_ipython_extension(ip: InteractiveShell) -> None:
+    "Load timer."
+    ip.events.register("pre_run_cell", timer.start)
+    ip.events.register("post_run_cell", timer.stop)
+
+
+def unload_ipython_extension(ip: InteractiveShell) -> None:
     "Unload timer."
-    ip.events.unregister('pre_run_cell', timer.start)
-    ip.events.unregister('post_run_cell', timer.stop)
+    ip.events.unregister("pre_run_cell", timer.start)
+    ip.events.unregister("post_run_cell", timer.stop)
