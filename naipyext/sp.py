fe3fb00287880eeb7aa5b56fcbf866c47438dcd9
Nasy <nasyxx+git@gmail.com>
2019-04-30 09:35:53 +0800

    Add stackprinter.

---
+++ naipyext/sp.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+r"""
+Life's pathetic, have fun ("▔□▔)/hi~♡ Nasy.
+
+Excited without bugs::
+
+    |             *         *
+    |                  .                .
+    |           .
+    |     *                      ,
+    |                   .
+    |
+    |                               *
+    |          |\___/|
+    |          )    -(             .              ·
+    |         =\ -   /=
+    |           )===(       *
+    |          /   - \
+    |          |-    |
+    |         /   -   \     0.|.0
+    |  NASY___\__( (__/_____(\=/)__+1s____________
+    |  ______|____) )______|______|______|______|_
+    |  ___|______( (____|______|______|______|____
+    |  ______|____\_|______|______|______|______|_
+    |  ___|______|______|______|______|______|____
+    |  ______|______|______|______|______|______|_
+    |  ___|______|______|______|______|______|____
+
+author   : Nasy https://nasy.moe
+date     : Apr 30, 2019
+email    : Nasy <nasyxx+python@gmail.com>
+filename : sp.py
+project  : naipyext
+license  : GPL-3.0+
+
+There are more things in heaven and earth, Horatio, than are dreamt.
+ --  From "Hamlet"
+"""
+# Other Packages
+from IPython.core.interactiveshell import InteractiveShell
+
+
+def load_ipython_extension(_: InteractiveShell) -> None:
+    """Load stackprinter."""
+    import stackprinter
+
+    stackprinter.set_excepthook(style="darkbg2")
